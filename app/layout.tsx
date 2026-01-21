03a255867a2b21bb69a5d286afb9d6addcbb5b7c
Ahmadfzal <113408830+Ahmadfzal@users.noreply.github.com>
2026-01-21 13:37:21 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [client] = useState(new QueryClient());
+
+  return (
+    <html lang="id">
+      <body>
+        <QueryClientProvider client={client}>
+          {children}
+        </QueryClientProvider>
+      </body>
+    </html>
+  );
+}
